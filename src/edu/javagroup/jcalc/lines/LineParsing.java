2f140962bc7c1a12b1a0937bd0aefd6d7eaf0469
aleco <sasha.sad@mail.ru>
2022-05-11 23:35:07 +0300

    11/05/22 23.35

--- src/edu/javagroup/jcalc/lines/LineParsing.java
+++ src/edu/javagroup/jcalc/lines/LineParsing.java
@@ -2,10 +2,33 @@ package edu.javagroup.jcalc.lines;
 
 public class LineParsing {
 
-    public static int getInteger(String str){
+    public static String findFirstMathSymbol(String str) {
+        return "";
+    }
+
+    public static String findFirstMathSymbol(String str, int priority) {
+        return "";
+    }
+
+    public String getNumberFromLeftPart(String str, int coordinate) {
+        return "";
+    }
+
+    public String getNumberFromRightPart(String str, int coordinate) {
+        return "";
+    }
+
+    public static int getInteger(String str) {
         return Integer.parseInt(str);
     }
-    public static double getDouble(String str){
+
+    public static double getDouble(String str) {
         return Double.parseDouble(str);
     }
+
+    public static boolean isFinalNumber(String str){
+        return false;
+    }
+
+
 }
