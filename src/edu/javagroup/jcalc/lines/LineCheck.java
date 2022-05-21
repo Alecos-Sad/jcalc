7088d16901569afb8e22e5d33d0c29d74cedd649
aleco <sasha.sad@mail.ru>
2022-05-22 00:23:04 +0300

    22.05.22 00.22

--- src/edu/javagroup/jcalc/lines/LineCheck.java
+++ src/edu/javagroup/jcalc/lines/LineCheck.java
@@ -2,32 +2,41 @@ package edu.javagroup.jcalc.lines;
 
 public class LineCheck {
 
-    public static boolean isLineCorrect(String str) {
-        return false;
+    public static boolean isLineCorrect(String source) {
+        return isRoundBracketsCorrect(source) && isMathSymbolsCorrect(source);
     }
 
-    public static boolean isRoundBracketsCorrect(String str) {
-        return false;
+    public static boolean isRoundBracketsCorrect(String source) {
+        return !source.contains("(") && !source.contains(")") || isRoundBracketsCountCorrect(source);
     }
 
-    public static boolean isRoundBracketsCountCorrect(String str){
-        return false;
+    public static boolean isRoundBracketsCountCorrect(String source) {
+        int openBracket = 0;
+        int closeBracket = 0;
+        for (int i = 0; i < source.length(); i++) {
+            openBracket = source.charAt(i) == '(' ? ++openBracket : openBracket;
+            closeBracket = source.charAt(i) == ')' ? ++closeBracket : closeBracket;
+        }
+        return openBracket == closeBracket &&  isRoundBracketsPositionsCorrect(source);
     }
 
-    public static boolean isRoundBracketsPositionCorrect(String str){
-        return false;
+    public static boolean  isRoundBracketsPositionsCorrect(String source) {
+        return !(source.indexOf(")") < source.indexOf("(")) || !(source.lastIndexOf("(") > source.lastIndexOf(")"));
+
     }
 
-    public static boolean isMathSymbolsCorrect(String str){
-        return false;
+    public static boolean isMathSymbolsCorrect(String source) {
+        return isFirstMathSymbol(source, "*") && isFirstMathSymbol(source, "/") && isLastMathSymbol(source, "*") &&
+                isLastMathSymbol(source, "/") && isFirstMathSymbol(source, "+") && isLastMathSymbol(source, "+") &&
+                isLastMathSymbol(source, "-");
     }
 
-    public static boolean isFirstMathSymbol(String str, String str1){
-        return false;
+    public static boolean isFirstMathSymbol(String source, String symbol) {
+        return !(source.trim().startsWith(symbol));
     }
 
-    public static boolean isLastMathSymbol(String str, String str1){
-        return false;
+    public static boolean isLastMathSymbol(String source, String symbol) {
+        return !(source.trim().endsWith(symbol));
     }
 
 
